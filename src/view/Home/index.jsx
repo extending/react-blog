bf7db1d3a017aa12ce650b97049a3ee102b59506
吕瑶 <lvyao91@sina.com>
2021-12-27 15:55:44 +0800

    feat：add styled-components

--- src/view/Home/index.jsx
+++ src/view/Home/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import styles from '../../css/common.module.css'
+
+const Cont = styled.h2`
+    background: purple
+`
 
 const Home = () => {
     return (
-        <div className={styles.child}>home...</div>
+        <>
+            <div className={styles.child}>home...</div>
+            <Cont>h2 title</Cont>
+        </>
     )
 }
 
